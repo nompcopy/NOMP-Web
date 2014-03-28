00a5d1ccf39aae2e9e4c12663a673e329ecba335
YanshuoH <hys235606751@gmail.com>
2014-03-28 16:10:15 +0100

    searchEngine with array key words and add array contains prototype utils

--- models/MatchingModel.js
+++ models/MatchingModel.js
@@ -33,17 +33,63 @@ MatchingModelSchema.methods = {
 
 // uills functions
 function calculateScore(ticket, key) {
-    var matching_result = {}
+    var matching_result = {};
     // We may use a iterator here but is annoying
     // name parse
     var term = ticket.name.match(new RegExp(key, 'ig'));
     var name_frequency = term ? term.length : 0;
     // description parse
-    term = ticket.description.match(new RegExp(data.keys, 'ig'));
+    term = ticket.description.match(new RegExp(key, 'ig'));
     var description_frequency = term? term.length : 0;
-    matching_result[ticket._id] = name_frequency * 1 + description_frequency * 0.2;
+    matching_result= {
+        id: ticket._id,
+        score: name_frequency * 1 + description_frequency * 0.2
+    };
 
     return matching_result;
+}
+
+
+
+// Merge results double array into one array and add up the scores
+function mergeResults(results) {
+    // I tried to do this recursively but I'm stupid
+    var arr = [];
+    // Merge arrays
+    for (var global_index=0; global_index<results.length; global_index++) {
+        for (var index=0; index<results[global_index].length; index++) {
+            var result = results[global_index][index];
+            arr.push(result);
+        }
+    }
+
+    var ids = [];
+    // get list of ids
+    for (var index=0; index<arr.length; index++) {
+        if (!ids.contains(arr[index].id)) {
+            ids.push(arr[index].id);
+        }
+    }
+
+    // initial new result array by list of ids
+    var new_result = [];
+    for (var index=0; index<ids.length; index++) {
+        new_result.push({
+            id: ids[index],
+            score: 0
+        });
+    }
+
+    // Add up the score from old results, unique the array
+    for (var id_index=0; id_index<new_result.length; id_index++) {
+        for (var index=0; index<arr.length; index++) {
+            if (new_result[id_index].id.toString() === arr[index].id.toString()) {
+                new_result[id_index].score += arr[index].score;
+            }
+        }
+    }
+
+    return new_result;
 }
 
 
@@ -53,30 +99,26 @@ exports.MatchingModel = mongoose.model('MatchingModel');
 
 // Async test
 var id = '533183829aa528f022af50a3';
-var keys = [];
-keys = 'test';
-var fields = ['name', 'description'];
-var queries = [];
 
 var data = {
-    keys: 'test',
+    keys: ['test', 'description', 'search'],
     fields: ['name', 'description']
 };
 
 
-// Search the tickets by key word from name and description
-function searchEngine(cb) {
+// Search the tickets by one key word from name and description
+function searchKeyWord(key, cb) {
 async.waterfall([
     // Search name field by keys
     function(callback) {
-        console.log('start async task');
-        NeedModel.findObjectByKeyword(data.fields[0], data.keys, function(err, name_results) {
+        console.log('Start One Key Word Search');
+        NeedModel.findObjectByKeyword(data.fields[0], key, function(err, name_results) {
             callback(null, name_results);
         });
     },
-    // Search description field by keys
+    // Search description field by key
     function(name_results, callback) {
-        NeedModel.findObjectByKeyword(data.fields[1], data.keys, function(err, description_results) {
+        NeedModel.findObjectByKeyword(data.fields[1], key, function(err, description_results) {
             var search_results = utils.union_arrays(name_results, description_results);
             callback(null, search_results);
         });
@@ -95,7 +137,7 @@ async.waterfall([
                 },
                 function(ticket, push_to_array) {
                     // TODO: need optimize, use another async.each to calculate scores and push back
-                    tickets.push(calculateScore(ticket, data.keys));
+                    tickets.push(calculateScore(ticket, key));
                     // We only callback the final result of array push
                     if (tickets.length == search_results.length) {
                         push_to_array (null, tickets);
@@ -111,7 +153,34 @@ async.waterfall([
     // cb = function(err, results) { do the process here};
 ], cb)}
 
-var t = function(err, callback) {
-    console.log('callback: ' + callback);
-};
-searchEngine(t);
+
+function searchKeyWords(cb) {
+    var results = [];
+    async.waterfall([
+        // Iterator keys
+        function(callback) {
+            console.log('Start iteration keys words search');
+            var index = 0;
+            async.each(data.keys, function(key, iterator_key_callback) {
+                searchKeyWord(key, function(err, one_key_results) {
+                    index ++;
+                    // We only care the final result to call back
+                    results.push(one_key_results);
+                    if (data.keys.length == index) {
+                        callback(null, results);
+                    }
+                });
+            });
+        },
+        // Add up the scores from results
+        function(results, callback) {
+            callback (null, mergeResults(results));
+        }
+    ], cb);
+}
+
+
+// Launch
+searchKeyWords(function(err, results) {
+    console.log(results);
+});
