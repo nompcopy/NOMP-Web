69ab344d8b4344f62645d1ef48a9c2c8333429ad
YanshuoH <hys235606751@gmail.com>
2014-04-01 13:54:56 +0200

    add authorization middleware

---
+++ config/middlewares/authorization.js
@@ -0,0 +1,27 @@
+/*
+ *  Generic require login routing middleware
+ */
+
+exports.requiresLogin = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    if (req.method == 'GET') {
+        req.session.returnTo = req.originalUrl;
+    }
+    res.redirect('/login');
+}
+
+/*
+ *  User authorization routing middleware
+ */
+
+exports.user = {
+  hasAuthorization: function (req, res, next) {
+    if (req.profile.id != req.user.id) {
+        req.flash('info', 'You are not authorized');
+        return res.redirect('/user/' + req.profile.id);
+    }
+    next();
+  }
+}
