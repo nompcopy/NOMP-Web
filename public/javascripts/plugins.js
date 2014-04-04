58d7f42ce6ece51e771834518ccb216616da69dd
YanshuoH <hys235606751@gmail.com>
2014-04-04 11:37:14 +0200

    add date plugins and complete index page

--- public/javascripts/plugins.js
+++ public/javascripts/plugins.js
@@ -4,8 +4,9 @@ var classificationData = [];
 $(document).ready(function() {
     populateClassificationList();
     populateActorTypeList();
-    populateOfferList();
-    populateNeedList();
+    populateTicketList();
+    // populateOfferList();
+    // populateNeedList();
 });
 
 
@@ -36,6 +37,11 @@ var displayFields = [
     { source_actor_type_name: 'Source: ' },
     { target_actor_type_name: 'Target: ' },
 ];
+var displayDates = [
+    { start_date: 'Start Date: ' },
+    { end_date: 'End Date: '},
+];
+
 
 function populateOfferList() {
     var tableContent = '';
@@ -55,7 +61,8 @@ function populateNeedList() {
     $.getJSON('/need/list', function(needs) {
         $.each(needs, function() {
             tableContent += '<li>';
-            tableContent += '<a href="/offer/' + this._id + '", title=' + this._name + '>' + this.name + '</a>';
+            tableContent += '<a href="/need/' + this._id + '", title=' + this._name + '>' + this.name + '</a>';
+            // class, actor types
             tableContent += '<ul>';
             for (var i=0; i<displayFields.length; i++) {
                 tableContent += '<li>';
@@ -65,9 +72,79 @@ function populateNeedList() {
                 tableContent += '</li>'
             }
             tableContent += '</ul>';
+            tableContent += '<p>';
+            tableContent += cutDescription(this.description);
+            tableContent += '</p>';
+            // dates
+            tableContent += '<ul>';
+            for (var i=0; i<displayDates.length; i++) {
+                tableContent += '<li>';
+                for (key in displayDates[i]) {
+                    tableContent += displayDates[i][key];
+                    tableContent += $.format.date(this[key], "dd/MM/yyyy");
+                }
+                tableContent += '</li>';
+            }
+            tableContent += '</ul>';
             tableContent += '</li>';
         });
         $('#needList ul').html(tableContent);
     });
-    console.log(classificationData);
 }
+
+
+function populateTicketList() {
+    var ticket_types = ['need', 'offer'];
+    for (var type_index=0; type_index<ticket_types.length; type_index++) {
+        var ticket_type = ticket_types[type_index];
+        $.getJSON('/' + ticket_type + '/list', function(tickets) {
+            var tableContent = '';
+            $.each(tickets, function() {
+                tableContent += '<li>';
+                tableContent += '<a href="/' + ticket_type + '/' + this._id + '", title=' + this._name + '>' + this.name + '</a>';
+                // class, actor types
+                tableContent += '<ul>';
+                for (var i=0; i<displayFields.length; i++) {
+                    tableContent += '<li>';
+                    for (key in displayFields[i]) {
+                        tableContent += displayFields[i][key] + this[key];
+                    }
+                    tableContent += '</li>'
+                }
+                tableContent += '</ul>';
+                tableContent += '<p>';
+                tableContent += cutDescription(this.description);
+                tableContent += '</p>';
+                // dates
+                tableContent += '<ul>';
+                for (var i=0; i<displayDates.length; i++) {
+                    tableContent += '<li>';
+                    for (key in displayDates[i]) {
+                        tableContent += displayDates[i][key];
+                        tableContent += $.format.date(this[key], "dd/MM/yyyy");
+                    }
+                    tableContent += '</li>';
+                }
+                tableContent += '</ul>';
+                tableContent += '</li>';
+            });
+            $('#' + parseUrl(this.url) + 'List ul').html(tableContent);
+        });
+    }
+}
+
+
+function cutDescription(description) {
+    if (description.length > 140) {
+        description = description.substr(0, 140);
+        description += '...';
+    }
+    return description;
+}
+
+function parseUrl(url) {
+    console.log(url);
+    var reg = new RegExp('\/(.*)\/', 'i');
+    return url.match(reg)[1];
+    // return arr;
+};
