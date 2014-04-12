a2d0e6d84fd296048615b26526939c5ea566498a
YanshuoH <hys235606751@gmail.com>
2014-04-12 14:44:40 +0200

    adapte matching results to restful

--- controllers/matchs.js
+++ controllers/matchs.js
@@ -9,22 +9,34 @@ var MatchingModel = mongoose.model('MatchingModel');
 
 exports.matching = function(req, res) {
     var ticket_type = req.params.type;
-    var ticket = req.ticket;
-    var m = new MatchingModel({
-        source_id: req.params.ticketId,
-        source_type: ticket_type,
-    });
-    m.matchEngine(function(err, results) {
-        var render_data = {
-            source_ticket: ticket,
-            ticket_type: ticket_type,
-            matching_results: results,
-            title: ticket.name,
-            req: req
-        };
-        return res.render('tickets/matching', render_data);
+    var ticket_id = req.params.ticketId;
+    // 1. Try to load an existing matching results
+    // 2. If not exist, create a new matching results
+    MatchingModel.retrieveByTicket(ticket_id, ticket_type, function(err, results) {
+        if (err || !results) {
+            var m = new MatchingModel({
+                source_id: ticket_id,
+                source_type: ticket_type
+            });
+            m.matchEngine(function(err, items) {
+                if (items.length == 0) {
+                    res.json([]);
+                }
+                else {
+                    m.results = items;
+                    m.save(function(err) {
+                        if (err) console.log(err);
+                        return res.json(m.results);
+                    });
+                }
+            });
+        }
+        else {
+            res.json(results.results);
+        }
     });
 }
+
 
 exports.searching = function(req, res) {
     var m = new MatchingModel();
