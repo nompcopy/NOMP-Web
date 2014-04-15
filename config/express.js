83c288680a37e3be7382432b3ec804d1073ce050
YanshuoH <hys235606751@gmail.com>
2014-04-15 22:23:59 +0200

    fix route 500 bug

--- config/express.js
+++ config/express.js
@@ -73,7 +73,7 @@ module.exports = function(app, config, passport) {
         // send emails if want
         console.error(err.stack)
         // error page
-        res.status(500).render('500', { error: err.stack });
+        res.status(500).render('500', { error: err.stack, req: req});
     });
     // assume 404 since no middleware responded
     app.use(function(req, res, next) {
