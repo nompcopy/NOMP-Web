b0a432b74beb7000c5c638b0999442ead53c183a
yipeng <huang.ypeng@gmail.com>
2014-05-03 23:38:25 +0200

    update admin view for class/actor list

--- public/javascripts/adminPlugin.js
+++ public/javascripts/adminPlugin.js
@@ -3,7 +3,6 @@ var actorTypeData = [];
 
 if (document.querySelector('#adminClassificationList')) {
     populateClassificationInfo();
-
 }
 if (document.querySelector('#adminActorTypeList')) {
     populateActorTypeInfo();
@@ -21,25 +20,30 @@ $('#adminActorTypeList table tbody').on('click', 'td a.linkshowactorinfo', showC
 function populateClassificationInfo() {
     var tableContent = '';
     $.getJSON('/classification/list', function(data) {
+        var dataInList = [];
         $.each(data, function() {
             classificationData = data;
             if (this.is_parent) {
                 return;
             }
+            dataInList.push(this);
             tableContent += '<tr>';
             tableContent += '<td>' + this.parent + '</td>';
             tableContent += '<td>' + this.parent_name + '</td>';
             tableContent += '<td>' + this._id + '</td>';
             tableContent += '<td><a href="#" class="linkshowclassinfo" rel="' + this._id + '">' + this.name + '</a></td>';
-            tableContent += '<td><form action="/admin/classification/delete/' + this._id +'" method="post">';
-            tableContent += '<input type="hidden" name="_method" value="DELETE">';
-            tableContent += '<input type="hidden" name="_csrf" value="'+ $('input[name="_csrf"]').val() +'">';
-            tableContent += '<button type="submit" class="btn btn-danger">Delete</button>';
-            tableContent += '</form></td>'
+            tableContent += '<td>';
+            var actionTypes = ['edit', 'delete'];
+            for (var i = 0; i < actionTypes.length; i++) {
+                tableContent +=  generateAdminAction($('input[name="_csrf"]').val(), 'classification', this._id, actionTypes[i]);
+            }
+            tableContent += '</td>';
             //tableContent += '<td><a href="#" class="linkdeleteclass" rel="' + this._id + '">Delete</a></td>';
             tableContent += '</tr>';
         });
         $('#adminClassificationList table tbody').html(tableContent);
+        
+        $('#adminClassificationList').append(generateListFooter(dataInList));
     });
 }
 
@@ -47,25 +51,30 @@ function populateClassificationInfo() {
 function populateActorTypeInfo() {
     var tableContent = '';
     $.getJSON('/actortype/list', function(data) {
+        var dataInList = [];
         $.each(data, function() {
             actorTypeData = data;
             if (this.is_parent) {
                 return;
             }
+            dataInList.push(this);
             tableContent += '<tr>';
             tableContent += '<td>' + this.parent + '</td>';
             tableContent += '<td>' + this.parent_name + '</td>';
             tableContent += '<td>' + this._id + '</td>';
             tableContent += '<td><a href="#" class="linkshowactorinfo" rel="' + this._id + '">' + this.name + '</a></td>';
-            tableContent += '<td><form action="/admin/actortype/delete/' + this._id +'" method="post">';
-            tableContent += '<input type="hidden" name="_method" value="DELETE">';
-            tableContent += '<input type="hidden" name="_csrf" value="'+ $('input[name="_csrf"]').val() +'">';
-            tableContent += '<button type="submit" class="btn btn-danger">Delete</button>';
-            tableContent += '</form></td>'
+            tableContent += '<td>';
+            var actionTypes = ['edit', 'delete'];
+            for (var i = 0; i < actionTypes.length; i++) {
+                tableContent +=  generateAdminAction($('input[name="_csrf"]').val(), 'actortype', this._id, actionTypes[i]);
+            }
+            tableContent += '</td>';
             //tableContent += '<td><a href="#" class="linkdeleteclass" rel="' + this._id + '">Delete</a></td>';
             tableContent += '</tr>';
         });
         $('#adminActorTypeList table tbody').html(tableContent);
+        
+        $('#adminActorTypeList').append(generateListFooter(dataInList));
     });
 }
 
@@ -150,4 +159,56 @@ function feedOptionJsonValue(obj) {
         name: obj.name
     }
     return value_stringify;
+}
+
+// generate action form for edit and delete action of a certain data (class or actor)
+function generateAdminAction(csrfToken, dataType, dataId, actionType) {
+    if (dataType === undefined || (dataType != 'actortype' && dataType != 'classification') || dataId === undefined || actionType === undefined || (actionType != 'delete' && actionType != 'edit')) {
+        return false;
+    }
+    
+    // generate url for form action
+    var formActionUrl = '#';
+    if (actionType == 'edit') {
+        // TODO: decide the edit action logic and interaction
+        formActionUrl = '#'
+    } else if (actionType == 'delete') {
+        formActionUrl = '/admin/' + dataType + '/delete/' + dataId;
+    }
+    
+    // generate form method for hidden
+    var formMethod = actionType == 'delete' ? 'DELETE' : 'PUT';
+    
+    var formClass = 'admin-action-form-' + actionType;
+    
+    // generate submit icon class
+    var icon = 'Go';
+    if (actionType == 'edit') {
+        icon = '<span class="glyphicon glyphicon-pencil text-primary"></span>';
+    } else if (actionType == 'delete') {
+        icon = '<span class="glyphicon glyphicon-remove text-danger"></span>';
+    }
+    
+    var formContent = '<form class="' + formClass + '" role="form" method="POST" action="' + formActionUrl + '" style="display: inline-block;">';
+    formContent += '<input type="hidden" name="_method" value="' + formMethod + '">';
+    formContent += '<input type="hidden" name="_csrf" value="'+ csrfToken +'">';
+    formContent += '<button type="submit" class="btn btn-xs" title="' + actionType + '">' + icon + '</button>';
+    formContent += '</form>';
+    
+    return formContent;
+}
+
+function generateListFooter(data) {
+    // results count
+    var listFooter = '<div class="panel-footer"><strong>' + data.length + '&nbsp;';
+    if (data.length == 1) {
+        listFooter += 'result';
+    } else {
+        listFooter += 'results';
+    }
+    listFooter += '</strong></div>';
+    
+    // TODO: pager
+    
+    return listFooter;
 }
